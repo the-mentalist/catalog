8d9495fd9f99fc79c7f1f0b1420386acd233a9af
Navpreet Ahuja <Navpreet Ahuja>
2017-07-01 14:56:54 +0530

    working routes

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask import session as login_session, make_response
+
+import json
+import random
+import string
 
 # Initialize flask app
 app = Flask(__name__)
@@ -16,25 +21,40 @@ def items(item):
 
 # Route to display item description
 @app.route('/catalog/<item>/<sub_item>')
-def items(item, sub_item):
+def sub_item(item, sub_item):
 	return "Selected %s, %s" % (item, sub_item)
 
 # Route to edit items for catalog selected
 @app.route('/catalog/<sub_item>/edit')
-def items(sub_item):
+def edit_item(sub_item):
 	return "Selected %s" % sub_item
 
 # Route to delete items for catalog selected
 @app.route('/catalog/<sub_item>/delete')
-def items(sub_item):
+def delete_items(sub_item):
 	return "Selected %s" % sub_item
 
 # Route for login
-@app.route('/login/<provider>')
-def items(provider):
-	return "Selected %s" % provider
+@app.route('/login')
+def login():
+	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+	login_session['state'] = state
+	return state
+
+@app.route('/connect', methods = ['POST'])
+def connect():
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
 
 # Route to display items for catalog selected
 @app.route('/logout')
 def items():
 	return "disconnected"
+
+if __name__ == '__main__':
+	super_secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+	app.secret_key = super_secret_key
+	app.debug = True
+	app.run('0.0.0.0', port = 5000)
